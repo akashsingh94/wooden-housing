c0de360a737513b2b7208f900ec49de31abe2c5d
Akash Singh <asakashrohit8@gmail.com>
2024-11-14 18:24:44 +0530

    thumbnail implementation

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,5 @@
 import { Typography } from "@mui/material";
 import classNames from "classnames";
-import React from "react";
 
 export default function Footer() {
   return (
--- src/components/home-project/ProjectImageSlider.tsx
+++ src/components/home-project/ProjectImageSlider.tsx
@@ -2,6 +2,8 @@ import classNames from "classnames";
 
 import SectionTitle from "../display/SectionTitle";
 import "./ProjectImageSlider.scss";
+import Image from "next/image";
+import Image1 from "../../../public/images/img.webp";
 
 type Props = {
   className?: string;
@@ -12,7 +14,48 @@ export default function ProjectImageSlider(props: Readonly<Props>) {
   return (
     <div className={classNames("component--project-image-slider", className)}>
       <SectionTitle title="Project Images" />
-      <div className="active-image tw--h-[31rem]"></div>
+      <div className="active-image tw--h-[31rem] tw--relative">
+        <div
+          className={classNames(
+            "tw--grid tw--w-full tw--grid-cols-4",
+            "tw--absolute tw--bottom-[-75px] tw--gap-2 tw--p-4"
+          )}
+        >
+          <div
+            className={classNames(
+              "thumbnail",
+              "tw--border-[3px] tw--h-32 tw--rounded-lg"
+            )}
+          ></div>
+          <div
+            className={classNames(
+              "thumbnail",
+              "tw--border-[3px] tw--h-32 tw--rounded-lg"
+            )}
+          ></div>
+          <div
+            className={classNames(
+              "thumbnail",
+              "tw--border-[3px] tw--h-32 tw--rounded-lg"
+            )}
+          ></div>
+          <div
+            className={classNames(
+              "thumbnail",
+              "tw--border-[3px] tw--h-32 tw--rounded-lg",
+
+              "tw--flex tw--items-center tw--justify-center"
+            )}
+          >
+            <span className="tw--w-[85px] tw--text-xl tw--font-medium tw--text-white tw--text-center">
+              View More (7)
+            </span>
+          </div>
+          {/* <Image src={Image1} alt="" />
+          <Image src={Image1} alt="" />
+          <Image src={Image1} alt="" /> */}
+        </div>
+      </div>
     </div>
   );
 }
