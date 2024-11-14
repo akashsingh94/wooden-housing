7c86c3cba1aac65bf84f5b5469c1d0a6641b08a7
Akash Singh <asakashrohit8@gmail.com>
2024-11-14 17:57:19 +0530

    app footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,21 @@
+import { Typography } from "@mui/material";
+import classNames from "classnames";
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer
+      className={classNames(
+        "component--footer",
+        "tw--h-[365px] tw--bg-[#4B325A] tw--mt-14",
+        "tw--text-white"
+      )}
+    >
+      <div className="tw--flex tw--items-center tw--justify-center tw--size-full">
+        <Typography className="tw--font-semibold tw--text-2xl">
+          Footer will go here
+        </Typography>
+      </div>
+    </footer>
+  );
 }
